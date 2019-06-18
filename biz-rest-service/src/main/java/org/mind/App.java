eb519e111192625f0e2360433819e292cb1f369b
unknown <peiliusheng@163.com>
2019-06-18 22:54:31 +0800

    new

--- biz-rest-service/src/main/java/org/mind/App.java
+++ biz-rest-service/src/main/java/org/mind/App.java
@@ -2,7 +2,7 @@ package org.mind;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//show me the money
 @SpringBootApplication
 public class App
 {
